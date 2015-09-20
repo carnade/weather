5b1c9df3d31580c21e5431cdf0f751c7fe0b7c4a
Andreas <lb.andreas@gmail.com>
2015-09-20 20:29:59 +0200

    database fixes

--- index.php
+++ index.php
@@ -98,18 +98,10 @@ img {
 
 <p><?php 
 
-require 'database.php';
+    require 'database.php';
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "Weather";
+    $conn = get_db_connection();
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
     $sql = "SELECT * from data_latest order by type desc";
     $result = ($conn->query($sql));
 
