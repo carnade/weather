01c7b06527b7c4563582db410bf14b730127ab05
Andreas <lb.andreas@gmail.com>
2016-01-14 13:12:09 +0100

    Sctructure

---
+++ weather.js
@@ -0,0 +1,13 @@
+(function() {
+  var app = angular.module('lyckhem', []);
+
+  app.controller('weatherController', function(){
+    
+  });
+
+  var gems = [
+    { name: 'Azurite', price: 2.95 },
+    { name: 'Bloodstone', price: 5.95 },
+    { name: 'Zircon', price: 3.95 }
+  ];
+})();
